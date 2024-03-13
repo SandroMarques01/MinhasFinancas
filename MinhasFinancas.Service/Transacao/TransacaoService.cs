5f2d4a412d0aa60c1f105c7cb0b31e3af53ebaf0
Sandro MARQUES <sandro.marques@vinci-energies.net>
2024-03-13 14:36:42 -0300

    Inativação do Papel quando está com saldo 0

--- MinhasFinancas.Service/Transacao/TransacaoService.cs
+++ MinhasFinancas.Service/Transacao/TransacaoService.cs
@@ -5,21 +5,34 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using System;
+using MinhasFinancas.Service.Papel;
+using System.Linq;
+using MinhasFinancas.Repository.Papel;
 
 namespace MinhasFinancas.Service.Transacao
 {
     public class TransacaoService : BaseService, ITransacaoService
     {
         private readonly ITransacaoRepository _baseRepository;
+        private readonly IPapelRepository _papelRepository;
 
-        public TransacaoService(ITransacaoRepository baseRepository, INotificador notificador) : base(notificador)
+        public TransacaoService(ITransacaoRepository baseRepository, IPapelRepository papelRepository, INotificador notificador) : base(notificador)
         {
             _baseRepository = baseRepository;
+            _papelRepository = papelRepository;
         }
 
         public async Task Add(Infra.Models.Transacao obj)
         {
             if (!ExecutarValidacao(new TransacaoValidation(), obj)) return;
+
+            var transPapel = await _baseRepository.Get(x => x.PapelId == obj.PapelId);
+            if (transPapel.Sum(x=> x.Quantidade * (x.TipoTransacao == Infra.TipoTransacao.Venda ? -1 : 1)) == 0)
+            {
+                var papel = await _papelRepository.GetById(obj.PapelId);
+                papel.Ativo = false;
+                await _papelRepository.Update(papel);
+            }
 
             await _baseRepository.Add(obj);
             await _baseRepository.SaveChanges();
@@ -52,6 +65,14 @@ namespace MinhasFinancas.Service.Transacao
         public async Task Update(Infra.Models.Transacao obj)
         {
             if (!ExecutarValidacao(new TransacaoValidation(), obj)) return;
+
+            var transPapel = await _baseRepository.Get(x => x.PapelId == obj.PapelId);
+            if (transPapel.Sum(x => x.Quantidade * (x.TipoTransacao == Infra.TipoTransacao.Venda ? -1 : 1)) == 0)
+            {
+                var papel = await _papelRepository.GetById(obj.PapelId);
+                papel.Ativo = false;
+                await _papelRepository.Update(papel);
+            }
 
             await _baseRepository.Update(obj);
             await _baseRepository.SaveChanges();
